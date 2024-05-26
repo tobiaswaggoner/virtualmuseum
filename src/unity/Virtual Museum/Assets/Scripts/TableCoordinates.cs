34565202526410f1c032e0a2dbfd394dd3d13a27
Max Pöttke <max-poettke@gmx.de>
2024-05-26 17:40:20 +0200

    feat: made some optimization changes

--- src/unity/Virtual Museum/Assets/Scripts/TableCoordinates.cs
+++ src/unity/Virtual Museum/Assets/Scripts/TableCoordinates.cs
@@ -12,15 +12,17 @@ public class TableCoordinates : MonoBehaviour
     {
         await Task.Delay(5000);
         var temp = GetComponent<MeshFilter>();
-        Vector3[] verts = temp.mesh.vertices;
+        Vector3 vert1 = temp.mesh.vertices[0];
+        Vector3 vert2 = temp.mesh.vertices[temp.mesh.vertices.Length - 1];
         Vector4 globalOffsetToTable = transform.parent.position;
-        for(int i = 0; i < verts.Length; i ++){
-            var v = transform.localToWorldMatrix * verts[i] + globalOffsetToTable;
-            verts[i] = v;
-            verts[i].y = transform.position.y;
-        }
+        var v = transform.localToWorldMatrix * vert1 + globalOffsetToTable;
+        vert1 = v;
+        vert1.y = transform.position.y;
+        v = transform.localToWorldMatrix * vert2 + globalOffsetToTable;
+        vert2 = v;
+        vert2.y = transform.position.y;
         cSVInterpreter = GameObject.FindGameObjectWithTag("InputsAndLogic").GetComponent<CSVInterpreter>();
-        cSVInterpreter.UpdateDesiredCorners(verts[0], verts[verts.Length - 1]);
+        cSVInterpreter.UpdateDesiredCorners(vert1, vert2);
         cSVInterpreter.CalculateStuff();
         
     }
