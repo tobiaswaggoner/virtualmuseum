9289e4d483055899482a88510648e2ea1fb3236f
Tobias Waggoner <tobias.waggoner@gmail.com>
2024-06-28 13:08:02 +0200

    feat: Releases Page

--- src/server/virtualmuseum.web/virtualmuseum.web.api/Program.cs
+++ src/server/virtualmuseum.web/virtualmuseum.web.api/Program.cs
@@ -1,15 +1,30 @@
+using dotenv.net;
 using Microsoft.OpenApi.Models;
 using virtualmuseum.web.api.Components;
 using virtualmuseum.web.api.Services;
+using virtualmuseum.web.api.Services.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
+DotEnv.Load();
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+
+builder.Services.Configure<ReleaseServiceConfig>(builder.Configuration.GetSection("ReleaseService"));
+
 builder.Services.AddSingleton<IConfigurationRepository, ConfigurationRepository>();
 builder.Services.AddTransient<IMediaService, MediaService>();
+builder.Services.AddSingleton<IReleaseService, ReleaseService>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// add httpclient for release service
+builder.Services.AddHttpClient<ReleaseService>();
 
 // Add rest controllers
 builder.Services.AddControllers();
